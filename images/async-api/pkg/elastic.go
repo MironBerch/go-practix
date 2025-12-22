5847efbd8e8aba26279c77351aba598fcb5ba1a3
MironBerch <mironberchetov@gmail.com>
2025-12-22 10:13:24 +0300

    Add elastic client setup for `async-api`

---
+++ images/async-api/pkg/elastic.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+    "log"
+    "github.com/elastic/go-elasticsearch/v9"
+	"async-api/internal/config"
+)
+
+func SetupElasticClient(cfg config.Config) (*elasticsearch.Client, error) {
+	esCfg := elasticsearch.Config{
+        Addresses: []string{"http://" + cfg.Elastic.Host + ":" + cfg.Elastic.Port},
+        Username:  cfg.Elastic.User,
+        Password:  cfg.Elastic.Password,
+    }
+    
+    es, err := elasticsearch.NewClient(esCfg)
+    if err != nil {
+        return nil, log.Errorf("Error creating client: %s", err)
+    }
+
+    log.Println(es.Info())
+
+	return es, nil
+}
