364de4970519cff87c5e1c767d9b6fe9b9a1ad2d
MironBerch <mironberchetov@gmail.com>
2025-12-22 09:23:16 +0300

    refactor: start migrate `async-api` from Python to Go

---
+++ images/async-api/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "encoding/json"
+)
+
+type healthzResponse struct {
+  Message string `json:"message"`
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+  response := healthzResponse{
+    Message: "ok",
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(200)
+
+  if err := json.NewEncoder(w).Encode(response); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+}
+
+func main() {
+  http.HandleFunc("/healthz", healthzHandler)
+
+  log.Fatal(http.ListenAndServe(":3000", nil))
+}
